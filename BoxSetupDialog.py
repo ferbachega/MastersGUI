917ea0be0e9ce3845e381d0d206af8375c176c8c
Michele Silva <michele.silva@gmail.com>
2015-01-13 12:47:44 -0200

    Refatorar pymol e glarea para arquivo próprio

--- BoxSetupDialog.py
+++ BoxSetupDialog.py
@@ -27,6 +27,7 @@ import gtk
 import gtk.gtkgl
 
 import gobject
+import pymol2
 from pymol import cmd
 from WindowControl import *
 
@@ -39,6 +40,7 @@ from OpenGL.GLU import *
 from FileChooserWindow           import *
 from pymol import cmd
 from pymol.cgo import *
+from pymol_connector import PymolWindow
 
 '''
 if not sys.platform.startswith('win'):
@@ -64,244 +66,7 @@ Zero_pointery   = 0
 Zero_ViewBuffer = None
 Menu            = True
 
-def draw(glarea, event):
-    # Get surface and context
-    glcontext = glarea.get_gl_context()
-    gldrawable = glarea.get_gl_drawable()
 
-    # Start opengl context
-    if not gldrawable.gl_begin(glcontext):
-        return
-
-    # Actual drawing
-    global sprite, angle, zoom
-
-    # Clear screen
-    #rabbyt.clear((0.0, 0.0, 0.0))
-
-    # Render sprite
-    if sprite is not None:
-        sprite.rot = angle
-        sprite.scale = zoom
-        sprite.render()
-
-    # Flush screen
-    gldrawable.swap_buffers()
-    pymol.draw()
-    # End opengl context
-    gldrawable.gl_end()
-
-    return True
-# Resizing function
-
-def reshape(glarea, event):
-
-    reshape = event
-    reshape_x = reshape.width
-    reshape_y = reshape.height
-    pymol.reshape(reshape_x, reshape_y, 0)
-    pymol.idle()
-    # pymol.draw()
-
-    # Get surface and context
-    glcontext = glarea.get_gl_context()
-    gldrawable = glarea.get_gl_drawable()
-
-    # Start opengl context
-    if not gldrawable.gl_begin(glcontext):
-        return
-
-    # Get widget dimensions
-    x, y, width, height = glarea.get_allocation()
-
-    pymol.reshape(width, height, True)
-
-    # Reset rabbyt viewport
-    #rabbyt.set_viewport((width, height))
-    # rabbyt.set_default_attribs()
-
-    # End opengl context
-    pymol.draw()
-    gldrawable.swap_buffers()
-    gldrawable.gl_end()
-    #
-
-    return True
-
-# Initialization function
-def init(glarea):
-    print 'init'
-    # Get surface and context
-    glcontext = glarea.get_gl_context()
-    gldrawable = glarea.get_gl_drawable()
-
-    # Start opengl context
-    if not gldrawable.gl_begin(glcontext):
-        return
-
-    # Get widget dimensions
-    x, y, width, height = glarea.get_allocation()
-
-    # Reset rabbyt viewport
-    #rabbyt.set_viewport((width, height))
-    # rabbyt.set_default_attribs()
-
-    # Get sprite variable
-    global sprite
-
-    # Load sprite
-    #sprite = rabbyt.Sprite('sprite.png')
-
-    # End opengl context
-    gldrawable.gl_end()
-
-    return True
-
-# Idle function
-def idle(glarea):
-    # Get vars
-    global angle, zoom, zfactor
-
-    # Update angle
-    angle += 1.0
-    if angle > 359:
-        angle = 0.0
-
-    # Update zoom
-    if zoom > 10 or zoom < 1:
-        zfactor = -zfactor
-        zoom += zfactor
-
-    # Needed for synchronous updates
-    if glarea.window:
-        glarea.window.invalidate_rect(glarea.allocation, False)
-        glarea.window.process_updates(False)
-
-    return True
-
-# Map events function
-def map(glarea, event):
-    # print 'map'
-    # Add idle event
-    gobject.idle_add(idle, glarea)
-    return True
-
-def slabchange(button, event):
-    global slab
-    x, y, width, height = glarea.get_allocation()
-    if event.direction == gtk.gdk.SCROLL_UP:
-        step = 1.5
-        slab = slab + step
-        slab = slab + step
-        # if  slab >=100:
-        #   slab = 100
-    else:
-        step = -1.5
-
-        slab = slab + step
-        if slab <= -5:
-            slab = -5
-    pymol.cmd.clip('slab', slab)
-    #cmd.zoom(buffer = Buffer)
-    return step
-    pymol.button(button, 0, x, y, 0)
-    pymol.idle()
-
-def show_context_menu(widget, event):
-    x, y, state = event.window.get_pointer()
-    if clicado:
-        if event.button == 3:
-            widget.popup(None, None, None, event.button, event.time)
-
-def mousepress(button, event):
-    global ZeroX, ZeroY
-    
-    ZeroX, ZeroY, state = event.window.get_pointer()
-    
-    #print ZeroX, ZeroY
-    
-    x, y, width, height = glarea.get_allocation()
-
-        
-    if event.button == 3:
-        global clicado
-        clicado = True
-        #print 'gordao'
-        x, y, width, height = glarea.get_allocation()
-        #print x, y, width, height
-        mousepress = event
-        button = mousepress.button - 1
-        pointerx = int(mousepress.x)
-        pointery = int(mousepress.y)
-        calc_y = height - pointery
-        #print pointerx,pointery,calc_y
-        #cmd.zoom(buffer=calc_y)
-        pymol.button(button, 0, pointerx , calc_y, 0)
-
-        
-    if event.button != 3:
-        x, y, width, height = glarea.get_allocation()
-        mousepress = event
-        button = mousepress.button - 1
-        pointerx = int(mousepress.x)
-        pointery = int(mousepress.y)
-        calc_y = height - pointery
-        pymol.button(button, 0, pointerx, calc_y, 0)
-
-def mouserelease(button, event):
-    x, y, width, height = glarea.get_allocation()
-    mouserelease = event
-    button = mouserelease.button - 1
-    pointerx = int(mouserelease.x)
-    pointery = int(mouserelease.y)
-    calc_y = height - pointery
-    pymol.button(button, 1, pointerx, calc_y, 0)
-    
-def mousemove(button, event):
-    global clicado, Buffer,Zero_pointerx, Zero_pointery, Zero_ViewBuffer, Menu
-    x, y, width, height = glarea.get_allocation()
-    clicado = False
-    mousemove = event
-    pointerx = int(mousemove.x)
-    pointery = int(mousemove.y)
-
-    calc_y2  = (float(Zero_pointery - pointery))/10.0
-    calc_y   = height - pointery
-    
-    if clicado:
-        global ZeroY
-        #print 'a'
-        #print clicado
-        #print Menu
-        #Buffer = (calc_y2)
-        #_view   = cmd.get_view()
-        #
-        #print _view
-        #if Zero_ViewBuffer == None:
-        #   Zero_ViewBuffer = _view[11]
-        #
-        #_view11 = Zero_ViewBuffer - Buffer
-        #_view15 = _view[15]       - Buffer
-        #_view16 = _view[16]       + Buffer
-        #
-        #_view2 = (_view[0], _view[1], _view[2],
-        #         _view[3], _view[4], _view[5],
-        #         _view[6], _view[7], _view[8],
-        #         _view[9], _view[10],_view11,
-        #         _view[12],_view[13],_view[14],
-        #         _view15,  _view16,  _view[17])
-        #
-        #print Buffer, _view11, _view15,_view16
-        #cmd.set_view(_view2)
-        #Zero_pointerx   = pointerx
-        #Zero_pointery   = pointery
-        #Zero_ViewBuffer = _view11
-    #else:
-    pymol.drag(pointerx, calc_y, 0)
-    pymol.idle()
-
-def my_menu_func(menu):
-    print "Menu clicado"
 
 #def context_menu():
 #    builder = masters.builder
@@ -323,11 +88,6 @@ glconfig = gtk.gdkgl.Config(mode=(gtk.gdkgl.MODE_RGB |
 
 class BoxSetupDialog:
     """ Class doc """
-
-    def on_spinbutton_change_value(self, widget, event):
-        """ Function doc """
-        print 'teste'
-        self.DrawCell()
     
     def on_spinbutton_change_value2(self, widget, event= None):
         """ Function doc """
@@ -438,7 +198,6 @@ class BoxSetupDialog:
     def on_window1_delete_event (self,widget, data=None):
         """ Function doc """
         print 'on_window1_delete_event'
-        print dir(gtk)#.quit()
     
     def on_window1_destroy_event(self,widget, data=None):
         """ Function doc """
@@ -448,6 +207,8 @@ class BoxSetupDialog:
     
     def __init__(self, project=None, window_control=None, main_builder=None, filein = None):
         """ Class initialiser """
+ 
+        self.pymol_window = PymolWindow()
         self.project = project
         self.window_control = window_control
         #self.builder = gtk.Builder()
@@ -506,6 +267,8 @@ class BoxSetupDialog:
 
         #-------------------- config PyMOL ---------------------#
                                                                 #
+        pymol = self.pymol_window.pymol
+        glarea = self.pymol_window.glarea
         container = self.builder.get_object("container")        #
         pymol.start()                                           #
         cmd = pymol.cmd                                         #
@@ -610,24 +373,6 @@ class BoxSetupDialog:
 
 
 
-print "Creating object"
-glarea = gtk.gtkgl.DrawingArea(glconfig)
-glarea.set_size_request(600, 400)
-glarea.connect_after('realize', init)
-glarea.connect('configure_event', reshape)
-glarea.connect('expose_event', draw)
-glarea.connect('map_event', map)
-glarea.set_events(glarea.get_events() | gtk.gdk.BUTTON_PRESS_MASK | gtk.gdk.BUTTON_RELEASE_MASK |
-                  gtk.gdk.POINTER_MOTION_MASK | gtk.gdk.POINTER_MOTION_HINT_MASK | gtk.gdk.KEY_PRESS_MASK)                      
-
-glarea.connect("button_press_event", mousepress)      
-glarea.connect("button_release_event", mouserelease)  
-glarea.connect("motion_notify_event", mousemove)      
-glarea.connect("scroll_event", slabchange)
-glarea.set_can_focus(True)
-
-import pymol2
-pymol   = pymol2.PyMOL(glarea)
 #masters = MastersMain()
 #glarea.connect_object("button_release_event", show_context_menu, context_menu())
 #masters.run()

37ac58d7e724e3901146e76be58adb4acef7c4e4
Michele Silva <michele.s@icanlocalize.com>
2015-01-26 11:20:56 -0200

    Refresh UI in between simulation steps

--- MCwindow.py
+++ MCwindow.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 #  MCwindow.py
-#  
+#
 #  Copyright 2014 Labio <labio@labio-XPS-8300>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 # System
 import time
@@ -28,22 +28,18 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 import gtk.gtkgl
-import gobject
-import sys
 import os
-import json 
+import json
 
 
 # Imports
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from pprint import pprint
-import random as rdm 
 
 #GUI
 #from MastersNewProjectDialog import *
-from FileChooserWindow           import *
-from pymol import cmd
+from FileChooserWindow import *
 from pymol.cgo import *
 
 from py4j.java_gateway import JavaGateway
@@ -135,7 +131,7 @@ class MonteCarloSimulationWindow:
         #               MCWINDOW METHODS             #
         #--------------------------------------------#
     ''' Only buttons and widgets - glade widgets signals'''
-    
+
     def on_treeview_button_release_event  (self, tree, event):
         if event.button == 3:
             selection     = tree.get_selection()
@@ -145,18 +141,18 @@ class MonteCarloSimulationWindow:
                 #self.selectedID  = str(model.get_value(iter, 1))  # @+
                 self.selectedObj    = str(model.get_value(iter, 1))
                 self.selectedFormat = str(model.get_value(iter, 2))
-                
+
                 self.builder.get_object('TreeViewObjLabel').set_label('- ' +self.selectedObj+' -' )
-                
+
                 widget = self.builder.get_object('treeview_menu')
                 widget.popup(None, None, None, event.button, event.time)
-            
+
         if event.button == 1:
             print "Mostrar menu de contexto botao1"
 
     def on_button_RunMCSimulation_clicked (self, button):
         """ Function doc """
-        
+
         # Environment Variables
         title                            = self.builder.get_object('job_title_entry')                       .get_text()
         InitialTemperature               = self.builder.get_object('Initial_Temperature_entry')             .get_text()
@@ -185,12 +181,12 @@ class MonteCarloSimulationWindow:
                              'input_coords'      : self.InputFiles['input_coords'],
                              'spatial_restraints': None,
                              'SS_restraints'     : None
-                            } 
-                         
+                            }
+
         self.InputParamaters = {
                                  'title'        : title,
                                  'outputname'   : None ,
-                                 
+
                                  'Cell'         : {
                                                    "maxX": self.builder.get_object('cell_maxX_entry').get_text(),
                                                    "maxY": self.builder.get_object('cell_maxY_entry').get_text(),
@@ -199,7 +195,7 @@ class MonteCarloSimulationWindow:
                                                    "minY": self.builder.get_object('cell_minY_entry').get_text(),
                                                    "minZ": self.builder.get_object('cell_minZ_entry').get_text()
                                                    },
-                                            
+
                                  'MCparameters' :{
                                                    'temperature'                            : InitialTemperature               ,
                                                    'temp_thr'                               : Temperature_THR                  ,
@@ -215,7 +211,7 @@ class MonteCarloSimulationWindow:
                                                    'max_crank'                              : AddWeightPivotCrank              ,
                                                    'min_pivot_dist'                         : MinPivotDistance                 ,
                                                    'max_angle'                              : MaxAngle                         ,
-                                                   'temp_factor_search'                     : TemperatureFactorSearch         
+                                                   'temp_factor_search'                     : TemperatureFactorSearch
                                                    },
                                  }
 
@@ -229,12 +225,12 @@ class MonteCarloSimulationWindow:
         pprint(self.InputFiles)
         pprint(self.InputParamaters)
         pprint(self.OutputParameters)
-        
+
 
         self.RunMastersMCSimulation()
-        
-        
-        
+
+
+
         #--------------------------------------------#
         #             MONTECARLO METHODS             #
         #--------------------------------------------#
@@ -244,36 +240,36 @@ class MonteCarloSimulationWindow:
         # - - - Ceating a new folder - - - #
         Job       = str(len(self.project['Jobs'])) # the new job ID
         newfolder = os.path.join(self.project['Folder'],Job +'_MonteCarlo')  # eg. /home/LABIO/Workspace/myself.project/1_MonteCarlo
-        if not os.path.exists (newfolder): 
-            os.mkdir (newfolder) 
+        if not os.path.exists (newfolder):
+            os.mkdir (newfolder)
         self.folder = newfolder
 
     def GenerateMastersMCInputFiles (self, InputFileName = None, OutputFileName = None):
         """ Function doc """
         parameters   = self.InputParamaters['MCparameters']
         input_coords = self.InputFiles['input_coords']
-        
-        
+
+
         Job          = str(len(self.project['Jobs'])) # the new job ID
-        
+
         if OutputFileName == None:
             self.InputParamaters['outputname'] = Job +'_MonteCarlo'
         else:
             self.InputParamaters['outputname'] = OutputFileName
 
-        
+
         #project   = self.Session.projects[self.Session.ActivedProject]
-        
-        '''                                    
+
+        '''
         --- ---------------------------------- ---
         ---            Input File              ---
         --- ---------------------------------- ---
         '''
-         
+
         arq          = open(InputFileName, 'w')
-        
+
         #input_coords = self.project['Jobs']['0']['Output']   - Michele's version
-       
+
         #----------------------------------INPUT-PARAMETERS---------------------------------------#
         text = '#  - - MASTERS input file simulation - - \n'                                      #
         text = str(text)                                                                          #
@@ -292,8 +288,8 @@ class MonteCarloSimulationWindow:
         text =  text + '\n\n'                                                                     #
         #-----------------------------------------------------------------------------------------#
 
-        
-        
+
+
         #-----------------------------CELL-PARAMETERS------------------------------------#
         text =  text + '# - - CELL-PARAMETERS - - \n'                                    #
         text =  text + 'max_pxcor = ' + str(self.InputParamaters['Cell']["maxX"]) + '\n' #
@@ -304,15 +300,15 @@ class MonteCarloSimulationWindow:
         text =  text + 'min_pzcor = ' + str(self.InputParamaters['Cell']["minZ"]) + '\n' #
         text =  text + '\n\n'                                                            #
         #--------------------------------------------------------------------------------#
-        
-        
-        
+
+
+
         #-------------------------------MCPARAMETERS------------------------------#
         text =  text + '# - - PARAMETERS - - \n'                                  #
         for i in parameters:                                                      #
             text =  text + i + ' = ' + parameters[i] + '\n'                       #
         #-------------------------------------------------------------------------#
-        
+
         arq.writelines(text)
         arq.close()
         return InputFileName
@@ -329,76 +325,83 @@ class MonteCarloSimulationWindow:
         InputFileName = os.path.join(self.folder, Job +'_MonteCarlo.in')   #
         self.GenerateMastersMCInputFiles(InputFileName)                    #
         #------------------------------------------------------------------#
-        
-        
+
+
         # - - - - - RUN MC SIMULATION - - - - -#
         gateway = JavaGateway()
         masters = gateway.entry_point.getMasters()
         masters.loadParameters(InputFileName)
-        
-        
-        
+
+
+
         print 'Starting simulation'
-        step = 0
-        
+        step = 1
+
         self.Session.projects[self.Session.ActivedProject]['Jobs'][Job] = {
                                      'Title'      : self.InputParamaters['title'],
-                                     'Folder'     : self.folder                  ,                
+                                     'Folder'     : self.folder                  ,
                                      'Input'      : InputFileName                ,
-                                     'Type'       : 'MonteCarlo'                 , 
+                                     'Type'       : 'MonteCarlo'                 ,
                                      'Status'     : 'running'                    ,
-                                     'Energy'     : '  -  '                      ,                   
-                                     'Start'      : '  -  '                      ,                 
+                                     'Energy'     : '  -  '                      ,
+                                     'Start'      : '  -  '                      ,
                                      'End'        : 'running'                    ,
                                      'Energy'     : '  -  '                      ,
                                      'parameters' : self.InputParamaters['MCparameters']
-                                     }              
-        
-        
+                                     }
+
+
         projectID  = self.Session.ActivedProject
         Jobs       = self.Session.projects[projectID]['Jobs']
         liststore  = self.Session.builder.get_object("liststore1")
         pprint(self.Session.projects[self.Session.ActivedProject]['Jobs'][Job])
-        
 
-        
+
+
         self.Session.WindowControl.AddJobHistoryToTreeview(liststore, Jobs)
-        
-        
+
+
         while masters.is_running():
             masters.step()
-        
+
+
+            # Update UI
+            while gtk.events_pending():
+                gtk.main_iteration(False)
+
             # ---------- adicionar aqui  tudo que  sera gerado de arquivos --------- #
-            try:                                                                     #
-                os.rename( os.path.join(self.folder,title+'-current.pdb'),           #
-                          os.path.join(self.folder,title+'_step_'+str(step+1)))      #
-            except:                                                                  #
-                pass                                                                 #
+            try:
+                os.rename(
+                    os.path.join(self.folder,title+'-current.pdb'),
+                    os.path.join(self.folder,title+'_step_' + step)
+                )
+            except:
+                pass
             # ---------------------------------------------------------------------- #
-            
+
             step += 1
         self.Session.projects[self.Session.ActivedProject]['Jobs'][Job]['End'] = 'finished'
         self.STATUSBAR_SET_TEXT('finished' )
         #project['Jobs'][Job] = {
         #            'Title'      : InputParamaters['title'],
-        #            'Folder'     : newfolder               ,                
+        #            'Folder'     : newfolder               ,
         #            'Input'      : inputfile               ,
         #            'InputCoord' : None                    ,
         #            'Output'     : filename_out            ,
         #            'OutputCoord': None                    ,
-        #            'LogFile'    : ''                      , 
-        #            'Type'       : 'MonteCarlo'            , 
-        #            'Energy'     : str(rdm.random()*1.2345),                   
-        #            'Start'      : '  -  '                 ,                 
+        #            'LogFile'    : ''                      ,
+        #            'Type'       : 'MonteCarlo'            ,
+        #            'Energy'     : str(rdm.random()*1.2345),
+        #            'Start'      : '  -  '                 ,
         #            'End'        : '  -  '                 ,
-        #            'parameters' : parameters}              
+        #            'parameters' : parameters}
         #Jobs      = self.project['Jobs']
         #liststore = self.main_builder.get_object("liststore1")
         #try:
         #    self.WindowControl.AddJobHistoryToTreeview(liststore, Jobs)
         #except:
         #    pass
-        #    
+        #
         HOME   = os.environ.get('HOME')
         FOLDER = HOME +'/.config/MASTERS/'
         json.dump(self.Session.projects, open(FOLDER + 'ProjectHistory.dat', 'w'), indent=2)
@@ -410,7 +413,7 @@ class MonteCarloSimulationWindow:
 
 def main():
 	window = MonteCarloSimulationWindow()
-    
+
 	return 0
 
 if __name__ == '__main__':
